d2570d3dd99e8ef1ff25b065c71d062c281300d2
Mario Souto <mariosoutoskn@gmail.com>
2021-10-06 12:50:46 -0300

    Update index.test.js

--- frontend/src/components/Text/index.test.js
+++ frontend/src/components/Text/index.test.js
@@ -3,7 +3,7 @@ import { render } from '../../infra/test/testUtils';
 
 describe('<Text />', () => {
     it('displays the component', () => {
-        const { container } = render(<Text>Text</Text>);
+        const { container } = render(<Text>Texta</Text>);
         expect(container).toMatchSnapshot();
     });
 });
